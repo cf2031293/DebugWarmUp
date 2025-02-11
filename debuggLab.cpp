8d84b27140f3786063748d066b8ab7b5f02312d2
StringKs <kent.phipps@ufl.edu>
2025-02-11 13:06:02 -0500

    first commit

---
+++ debuggLab.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void computeSum(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i <= size; i++) {
+        sum += arr[i];
+    }
+    std::cout << "Sum: " << sum << std::endl;
+}
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    computeSum(numbers, size);
+
+    return 0;
+}
