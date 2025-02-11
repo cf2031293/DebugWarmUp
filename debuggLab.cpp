7a3e3b67ace16e33e1f0b59c3b88f30176bbafb9
cf2031293 <chenf1@ufl.edu>
2025-02-11 14:16:11 -0500

    edited the inequality from <= to <

--- debuggLab.cpp
+++ debuggLab.cpp
@@ -2,7 +2,7 @@
 
 void computeSum(int arr[], int size) {
     int sum = 0;
-    for (int i = 0; i <= size; i++) {
+    for (int i = 0; i < size; i++) { // edited the inequality from <= to <
         sum += arr[i];
     }
     std::cout << "Sum: " << sum << std::endl;
